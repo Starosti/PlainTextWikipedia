ff4fabc0ebe08d0bf0900c6da924405d1916a8bd
Starosti <46249941+Starosti@users.noreply.github.com>
2023-07-27 12:00:32 +0300

    edit for single file and trwiki

--- dewiki_functions.py
+++ dewiki_functions.py
@@ -4,11 +4,13 @@ import re
 from html2text import html2text as htt
 import wikitextparser as wtp
 
+whole = list()
+
 
 def dewiki(text):
-    text = wtp.parse(text).plain_text()  # wiki to plaintext 
+    text = wtp.parse(text).plain_text()  # wiki to plaintext
     text = htt(text)  # remove any HTML
-    text = text.replace('\\n',' ')  # replace newlines
+    text = text.replace('\\n', ' ')  # replace newlines
     text = re.sub('\s+', ' ', text)  # replace excess whitespace
     return text
 
@@ -25,7 +27,8 @@ def analyze_chunk(text):
             if ':' in title:  # most articles with : in them are not articles we care about
                 return None
         serial = text.split('<id>')[1].split('</id>')[0]
-        content = text.split('</text')[0].split('<text')[1].split('>', maxsplit=1)[1]
+        content = text.split(
+            '</text')[0].split('<text')[1].split('>', maxsplit=1)[1]
         content = dewiki(content)
         return {'title': title.strip(), 'text': content.strip(), 'id': serial.strip()}
     except Exception as oops:
@@ -36,19 +39,30 @@ def analyze_chunk(text):
 def save_article(article, savedir):
     doc = analyze_chunk(article)
     if doc:
-        print('SAVING:', doc['title'])
-        filename = doc['id'] + '.json'
-        with open(savedir + filename, 'w', encoding='utf-8') as outfile:
-            json.dump(doc, outfile, sort_keys=True, indent=1, ensure_ascii=False)
+        # print('SAVING:', doc['title'])
+        whole.append(doc)
+        # with open(savedir + filename, 'w', encoding='utf-8') as outfile:
+        #    json.dump(doc, outfile, sort_keys=True, indent=1, ensure_ascii=False)
 
 
 def process_file_text(filename, savedir):
     article = ''
     with open(filename, 'r', encoding='utf-8') as infile:
-        for line in infile:
+        print("counting lines...")
+        num_lines = sum(1 for _ in open(filename, 'r', encoding='utf-8'))
+        print("total lines:", num_lines)
+        for i, line in enumerate(infile):
             if '<page>' in line:
                 article = ''
             elif '</page>' in line:  # end of article
+                if (i % 100 == 0):
+                    prttxt = '{}/{} lines ({}%)'.format(i,
+                                                        num_lines, round(i/num_lines*100, 2))
+                    print(prttxt)
                 Thread(target=save_article, args=(article, savedir)).start()
             else:
-                article += line                
+                article += line
+    with open('wikiTR.json', 'w', encoding='utf-8') as output_file:
+        print("saving...")
+        json.dump(whole, output_file, sort_keys=True,
+                  indent=1, ensure_ascii=False)
--- wiki_to_text.py
+++ wiki_to_text.py
@@ -1,8 +1,8 @@
 from dewiki_functions import *
 
-#wiki_xml_file = 'F:/simplewiki-20210401/simplewiki-20210401.xml'  # update this
-wiki_xml_file = 'G:/enwiki-20210401/enwiki-20210401.xml'  # update this
-json_save_dir = 'G:/wiki_plaintext/'
+# wiki_xml_file = 'F:/simplewiki-20210401/simplewiki-20210401.xml'  # update this
+wiki_xml_file = 'trwiki-latest-pages-articles.xml'  # update this
+json_save_dir = 'wiki/'
 
 if __name__ == '__main__':
     process_file_text(wiki_xml_file, json_save_dir)
